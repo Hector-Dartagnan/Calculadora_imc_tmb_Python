2b3a02836f65179500355d32889487b1cc3fa85c
Hector Dartagnan <brumdartagnan@gmail.com>
2026-09-12 04:36:44 -0300

    Adicionado Funções: Calcular imc, Calcular TMB, Tabela

--- TMB_Principal.py
+++ TMB_Principal.py
@@ -1,3 +1,40 @@
+def calcular_imc():
+
+    peso = float(input("Digite o seu peso em kg: "))
+    altura = float(input("Digite sua altura em centímetros: "))
+    imc = peso / ((altura / 100) ** 2)
+
+    return imc
+
+def calcular_tmb():
+    peso = float(input("Digite o seu peso em KG: "))
+    altura = int(input("Digite sua altura em CM: "))
+    idade = int(input("Digite sua idade: "))
+    sexo = input("Digite seu sexo (M/F): ")
+
+    if sexo.upper() == "M":
+        tmb = (10 * peso) + (6.25 * altura) - (5 * idade) + 5
+    elif sexo.upper() == "F":
+        tmb = (10 * peso) + (6.25 * altura) - (5 * idade) - 161
+    else:
+        print("Sexo inválido. Por favor, digite M para masculino ou F para feminino.")
+        return
+
+    print(f"Sua Taxa Metabólica Basal é: {tmb:.2f} kcal / dia")
+
+def tabela(imc):
+
+    print(f"Seu IMC é: {imc:.2f}")
+    if imc < 18.5:
+        print("Você está abaixo do peso.")
+    elif imc < 25:
+        print("Você está com peso normal.")
+    elif imc < 30:
+        print("Você está com sobrepeso.")
+    elif imc < 35:
+        print("Você está com obesidade grau I.")
+    elif imc < 40:
+        print("Você está com obesidade grau II.")
 
 def escolha():
 
@@ -5,12 +42,13 @@ def escolha():
     print("1. Calcular IMC")
     print("2. Calcular TMB")
 
-    n = int(input("Opção: "))
+    opcao = int(input("Opção: "))
 
-    if n == 1:
-        imc()
-    elif n == 2:
-        tmb()
+    if opcao == 1:
+        imc = calcular_imc()
+        tabela(imc)
+    elif opcao == 2:
+        calcular_tmb()
 
 def principal():
 
@@ -18,9 +56,8 @@ def principal():
 
     escolha()
 
-principal()
-
-
+if __name__ == "__main__":
+    principal()
 
 
 
