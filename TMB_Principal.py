901b65188f4d7a67dfae985085bca8a8273bedc6
Hector Dartagnan <brumdartagnan@gmail.com>
2026-09-16 16:34:13 -0300

    Removido função tabela() agora o cálculo da tabela acontece na mesma função do cálculo imc() Simplificação do codigo e limpeza para melhor legibilidade

--- TMB_Principal.py
+++ TMB_Principal.py
@@ -4,7 +4,18 @@ def calcular_imc():#função para calcular o IMC do usuário.
     altura = float(input("Digite sua altura em centímetros: "))
     imc = peso / ((altura / 100) ** 2)#Aqui os dados inseridos pelo usuário são usados para calcular o IMC.
 
-    return imc#Método retorna o valor do IMC calculado para a função que chamou calcular_imc().
+    if imc < 18.5:#O método if verifica em qual faixa de IMC o usuário se encontra e exibe a classificação correspondente.
+            return print(f"Seu IMC é {imc:.2f}. Você está abaixo do peso.")
+    elif imc < 25:
+            return print(f"Seu IMC é {imc:.2f}. Você está com peso normal.")
+    elif imc < 30:
+            return print(f"Seu IMC é {imc:.2f}. Você está com sobrepeso.")
+    elif imc < 35:
+            return print(f"Seu IMC é {imc:.2f}. Você está com obesidade grau I.")
+    elif imc < 40:
+            return print(f"Seu IMC é {imc:.2f}. Você está com obesidade grau II.")
+    else:
+            return print(f"Seu IMC é {imc:.2f}. Você está com obesidade grau III.")
 
 def calcular_tmb():#função para calcular a TMB do usuário.    
     
@@ -12,8 +23,7 @@ def calcular_tmb():#função para calcular a TMB do usuário.
     altura = float(input("Digite sua altura em CM: "))
     idade = float(input("Digite sua idade: "))
     sexo = input("Digite seu sexo (M/F): ")
-    nivel = int(input("Digite seu nível de atividade física:\n1. Sedentário\n2. Levemente ativo (1 a 3 x por semana)\n3. Moderadamente ativo (3 a 5 x por semana)\n4. Muito ativo (6 a 7 x por semana): "))
-
+    
 #Cálculo da TMB usando a fórmula de Mifflin-St Jeor, que varia dependendo do sexo do usuário.
 
     if sexo.upper() == "M":
@@ -23,7 +33,15 @@ def calcular_tmb():#função para calcular a TMB do usuário.
     else:#Método para tratar entradas inválidas de sexo, caso o usuário digite algo diferente de M ou F.
         print("Sexo inválido. Por favor, digite M para masculino ou F para feminino.")
         return 
-
+    
+    print('''Digite seu nivel de atividade fisica:
+          1. Sedentário
+          2. Levemente ativo (1 a 3 x por semana)
+          3. Moderadamente ativo (3 a 5 x por semana)
+          4. Muito ativo (6 a 7 x por semana)''')
+    
+    nivel = int(input("Opção: "))
+    
     #Cálculo da TMB ajustada pelo nível de atividade física do usuário, multiplicando a TMB calculada pelo fator correspondente ao nível de atividade.
     fatores = {1: 1.2, 2: 1.375, 3: 1.55, 4: 1.725}#Dicionário que associa cada nível de atividade física a um fator multiplicador para ajustar a TMB.
     if nivel in fatores:
@@ -34,25 +52,8 @@ def calcular_tmb():#função para calcular a TMB do usuário.
 
 #O método print() exibe a TMB calculada para o usuário, formatando o valor com duas casas decimais.
     print(f"Sua Taxa Metabólica Basal é: {tmb:.2f} kcal / dia")
-    
-def tabela(imc):
-    
-    #função para exibir a classificação do IMC com base no valor calculado seguindo as diretrizes da OMS.
-    print(f"Seu IMC é: {imc:.2f}")
-    if imc < 18.5:#O método if verifica em qual faixa de IMC o usuário se encontra e exibe a classificação correspondente.
-        print("Você está abaixo do peso.")
-    elif imc < 25:
-        print("Você está com peso normal.")
-    elif imc < 30:
-        print("Você está com sobrepeso.")
-    elif imc < 35:
-        print("Você está com obesidade grau I.")
-    elif imc < 40:
-        print("Você está com obesidade grau II.")
-    else:
-        print("Você está com obesidade grau III.")
 
-def escolha():#função para permitir que o usuário escolha entre calcular IMC ou TMB.
+def menu():#função para permitir que o usuário escolha entre calcular IMC ou TMB.
 #A função exibe um menu de opções e chama a função correspondente com base na escolha do usuário.
 
     while True:
@@ -67,10 +68,9 @@ def escolha():#função para permitir que o usuário escolha entre calcular IMC 
         print("Opção inválida. Por favor, digite um número inteiro.")
         continue
         
-#Se a opção escolhida for 1, chama a função calcular_imc() e exibe a tabela de classificação do IMC.
+#Se a opção escolhida for 1, chama a função calcular_imc().
      if opcao == 1:
-          imc = calcular_imc()#Guarda o valor do IMC calculado em uma variável.
-          tabela(imc)#Envia o valor do IMC para a função tabela() para exibir a classificação do IMC.
+          calcular_imc()
      elif opcao == 2:# Se a opção escolhida for 2, chama a função calcular_tmb().
           calcular_tmb()
      elif opcao == 3:#Se a opção escolhida for 3, encerra o programa.
@@ -81,9 +81,9 @@ def escolha():#função para permitir que o usuário escolha entre calcular IMC 
 
 def principal():#função principal do programa que apresenta a finalidade da aplicação e chama a função escolha().
     
-    print("Olá! Esta é uma aplicação para calcular seu IMC e Taxa Metabólica Basal (TMB).")
+    print("Olá! Esta é uma aplicação para calcular seu indice de massa corporal (IMC) e Taxa Metabólica Basal (TMB).")
 #Função que chama a função escolha() para permitir que o usuário escolha entre calcular IMC ou TMB.
-    escolha()
+    menu()
 
 if __name__ == "__main__":#Verifica se o script está sendo executado diretamente (não importado como módulo) e chama a função principal.
     principal()
